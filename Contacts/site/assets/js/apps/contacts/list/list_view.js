7c7b7362a335a185869334db87cffdd140a477da
Aaron Holmes <aholmes@halogensoftware.com>
2014-03-05 11:28:30 -0500

    Adding some simple events

--- Contacts/site/assets/js/apps/contacts/list/list_view.js
+++ Contacts/site/assets/js/apps/contacts/list/list_view.js
@@ -1,7 +1,16 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
 	List.Contact = Marionette.ItemView.extend({
 		tagName: "tr",
-		template: "#contact-list-item"
+		template: "#contact-list-item",
+
+		events: {
+			"click": "hightlightName"
+		},
+
+		hightlightName: function(e){
+			e.preventDefault();
+			this.$el.toggleClass("warning");
+		}
 	});
 
 	List.Contacts = Marionette.CompositeView.extend({
